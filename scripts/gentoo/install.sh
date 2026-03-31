65991e455aa314a25f41e0251ea0d912ab398782
John Berg <john.berg.96bits@gmail.com>
2026-03-31 22:23:20 +0100

    Rename the primary and secondary sets
    Now they become @main and @desktop, respectively. As this is more
    descriptive as to their purpose. And also add a little bit of a
    description in the files  themselves.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- scripts/gentoo/install.sh
+++ scripts/gentoo/install.sh
@@ -32,5 +32,5 @@ scripts/gentoo/configure-portage.sh
 
 echo "Install..." > /dev/stderr
 # Do the main installation and cleanup
-emerge --ignore-default-opts -uqDN @primary
+emerge --ignore-default-opts -uqDN @main
 emerge --ignore-default-opts --deplean
