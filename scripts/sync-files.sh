7296a72c5543e531c14763236e33ef0d18c7d461
John Berg <john.berg.96bits@gmail.com>
2026-03-31 22:01:59 +0100

    Improve the sync-files script for the /etc directory
    If we point this script at e.g. /etc/portage/ then this will pick up a
    lot of files whcih we do not want to sync. Such as temporary files or
    system specific files. Instead, we can use the --relative option in
    rsync to copy the entire path from the source /etc dir. This means we
    can exclude files based on absolute paths.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- scripts/sync-files.sh
+++ scripts/sync-files.sh
@@ -19,7 +19,24 @@ case $TARGET_PATH in
     rsync -avA --delete "${TARGET_PATH}" "${LOCAL_PATH}"
     ;;
 "/etc/"*)
-    LOCAL_PATH="$(dirname etc/${TARGET_PATH#/etc/})/"
-    rsync -avA --delete "${TARGET_PATH}" "${LOCAL_PATH}"
+    # Sync files into the ./etc directory relative to the absolute path. This
+    # makes it easy to filter out files and directoreis which we do not want to
+    # include. Such as files we want to generate or other system specific
+    # files.
+    rsync -avA --delete --relative                            \
+          --exclude='/etc/portage/make.conf'                  \
+          --exclude='/etc/portage/make.profile'               \
+          --exclude='/etc/portage/binrepos.conf/*'            \
+          --exclude='/etc/portage/package.use/.*'             \
+          --exclude='/etc/portage/package.use/cpuflags'       \
+          --exclude='/etc/portage/package.use/videocards'     \
+          --exclude='/etc/portage/postsync.d/*'               \
+          --exclude='/etc/portage/savedconfig/sys-firmware/*' \
+          --exclude='/etc/portage/savedconfig/sys-kernel/*'   \
+          "${TARGET_PATH}" "${PWD}/"
+    ;;
+*)
+    echo "Unsupported path!" >> /dev/stderr
+    exit 1
     ;;
 esac
