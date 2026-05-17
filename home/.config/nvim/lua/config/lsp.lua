cc580fe553f930efb561dcfa300e10be22b7e816
John Berg <john.berg.96bits@gmail.com>
2026-05-17 17:59:22 +0100

    Add a proper luarc file for the lua config files
    Quite plain and simple.
    
    Signed-off-by: John Berg <john.berg.96bits@gmail.com>

--- home/.config/nvim/lua/config/lsp.lua
+++ home/.config/nvim/lua/config/lsp.lua
@@ -45,16 +45,6 @@ local lsp_config = {
         cmd = { 'lua-language-server' },
         filetypes = { 'lua' },
         root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = { 'vim' },
-                },
-                runtime = {
-                    version = 'LuaJIT',
-                }
-            }
-        }
     },
     rust_analyzer = {
         cmd = { 'rust-analyzer' },
